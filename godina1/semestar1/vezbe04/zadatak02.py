ccc232d43b233d0b6eea256964ab845f95fe5ea6
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-11-28 13:18:16 +0100

    vezbe 4

---
+++ godina1/semestar1/vezbe04/zadatak02.py
@@ -0,0 +1,13 @@
+import os; os.system("cls")
+
+def obim_kruga(r):
+    return 2 * r * 3.14
+
+def povrsina_kruga(r):
+    return r ** 2 * 3.14
+
+obim_kruga = obim_kruga(1)
+povrsina_kruga = povrsina_kruga(1)
+
+print(obim_kruga)
+print(povrsina_kruga)
