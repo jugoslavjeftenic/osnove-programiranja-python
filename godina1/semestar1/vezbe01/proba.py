1ea33da98e0108de4a59f3b36003d4ddd199b089
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-11-28 13:16:01 +0100

    vezbe 1

---
+++ godina1/semestar1/vezbe01/proba.py
@@ -0,0 +1,6 @@
+print(4)
+print(4.3)
+print("4")
+print('4')
+ime_i_prezime = "Jugoslav Jeftenic"
+# neki komentar
---
+++ godina1/semestar1/vezbe01/zadatak02.py
@@ -0,0 +1,10 @@
+univerzitet = "Univerzitet Singidunum - Centar Novi Sad."
+godina = 2013
+akreditovan = True
+prosek = 10.0
+datum_kolokvijuma = None
+print(univerzitet)
+print(godina)
+print(akreditovan)
+print(prosek)
+print(datum_kolokvijuma)
